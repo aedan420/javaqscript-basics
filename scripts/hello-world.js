47593213e5d3ebb74f298216cd31304f6945d216
--global <--global>
2021-11-02 15:47:46 -0600

    adding variable file

---
+++ scripts/hello-world.js
@@ -0,0 +1,3 @@
+let fullName = "aedan crewes";
+let age = 3;
+console.log("hello world! my name is %s and i'm %d", fullName, age);
--- scripts/world.js
+++
@@ -1,4 +0,0 @@
-    // this is a comment hey this is how i define comments
-    // this is my world.js program
-    console.log("world.js
-    ")
